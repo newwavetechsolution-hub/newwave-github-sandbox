7f78637be7177900eaf42bdb12b966256c4bd7ae
MethinySamnang <samnangmethiny@gmail.com>
2026-02-13 17:51:15 +0700

    feat: add calc module

---
+++ phase-1-local-basics/exercise-2-commit-history/calc.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+if __name__ == "__main__":
+    # This block only runs if the script is executed directly
+    result = add(2, 3)
+    print(result)
